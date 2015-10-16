e06ce60412b72728a06e155d6b9c6de7c3fa20e6
mtmcgraw <mtmcgraw@mit.edu>
2015-10-16 14:42:16 -0400

    added toast event

--- app/src/main/java/com/mattmellor/gear/MainActivity.java
+++ app/src/main/java/com/mattmellor/gear/MainActivity.java
@@ -1,17 +1,22 @@
 package com.mattmellor.gear;
 
+import android.content.Context;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.Spannable;
 import android.text.style.BackgroundColorSpan;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -45,8 +50,15 @@ public class  MainActivity extends AppCompatActivity {
                 str.setSpan(new BackgroundColorSpan(0xFFFF),selection_start,selection_end, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
 
                 String text = txtContent.getText().toString();
-                String copy = text.substring(selection_start,selection_end);
-                
+                String copy = text.substring(selection_start, selection_end);
+
+                Context context = getApplicationContext();
+                CharSequence message = copy + " ausgewählt.";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, message, duration);
+                toast.setGravity(Gravity.TOP,0,0);
+                toast.show();
                 definition.setText(copy);
                 return false;
             }
