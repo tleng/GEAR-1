e814b713366adb851fa9987e9470f19ccceb0947
Matthew <mmellor@mit.edu>
2015-11-08 14:11:03 -0500

    made it look better

--- app/src/main/java/com/mattmellor/gear/popUpRateArticle.java
+++ app/src/main/java/com/mattmellor/gear/popUpRateArticle.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Matthew on 10/23/2015.
@@ -27,6 +28,15 @@ public class popUpRateArticle extends Activity {
 
 
     }
+
+    public String onClickRate(View view){
+        Button rate = (Button) view;
+        String rating = rate.getText().toString();
+        return rating;
+    }
+
+
+
 
     public void rateArticle(View view) {
         finish();
